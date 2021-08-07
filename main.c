7f37a5f8e7b21182b9b2f8ff96d7247f91bb9051
VIRUSLOKI <virusloki@ya.ru>
2021-08-07 18:15:41 +0300

    last commit

--- main.c
+++ main.c
@@ -1,43 +1,43 @@
 #include "philo.h"
-void ft_start_threads(t_table *t)
+void	ft_start_threads(t_table *t)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    t->start_life = ft_get_time();
-    while (i < t->num)
-    {
-        pthread_create(&t->philos[i].thread, NULL, start_routine,
-                       &t->philos[i]);
-        t->philos[i].start = ft_get_time();
-        pthread_create(&t->philos[i].monitor, NULL, start_monitoring,
-                       &t->philos[i]);
-        i++;
-    }
-    i = 0;
-    while (i < t->num)
-    {
-        pthread_join(t->philos[i].thread, NULL);
-        pthread_join(t->philos[i].monitor, NULL);
-        i++;
-    }
+	i = 0;
+	t->start_life = ft_get_time();
+	while (i < t->num)
+	{
+		pthread_create(&t->philos[i].thread, NULL, start_routine,
+			&t->philos[i]);
+		t->philos[i].start = ft_get_time();
+		pthread_create(&t->philos[i].monitor, NULL, start_monitoring,
+			&t->philos[i]);
+		i++;
+	}
+	i = 0;
+	while (i < t->num)
+	{
+		pthread_join(t->philos[i].thread, NULL);
+		pthread_join(t->philos[i].monitor, NULL);
+		i++;
+	}
 }
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_table t;
+	t_table	t;
 
-    if (argc != 5 && argc != 6)
-        return (-1);
-    if (!ft_init_args(&t, argc, argv))
-        return (1);
-    if (!ft_init_phil(&t))
-    {
-        if (t.philos)
-            free(t.philos);
-        return (1);
-    }
-    ft_start_threads(&t);
-    ft_destroy(&t);
-    return 0;
+	if (argc != 5 && argc != 6)
+		return (-1);
+	if (!ft_init_args(&t, argc, argv))
+		return (1);
+	if (!ft_init_phil(&t))
+	{
+		if (t.philos)
+			free(t.philos);
+		return (1);
+	}
+	ft_start_threads(&t);
+	ft_destroy(&t);
+	return (0);
 }
--- utils.c
+++ utils.c
@@ -1,67 +1,66 @@
 #include "philo.h"
-
 int	ft_isdigit(int c)
 {
-    if (c >= 48 && c <= 57)
-        return (1);
-    else
-        return (0);
+	if (c >= 48 && c <= 57)
+		return (1);
+	else
+		return (0);
 }
 
 int	ft_atoi(const char *str)
 {
-    int	i;
-    int	num;
-    int	sign;
+	int	i;
+	int	num;
+	int	sign;
 
-    i = 0;
-    num = 0;
-    sign = 1;
-    if (!*str)
-        return (0);
-    while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-        i++;
-    if (str[i] == '+' || str[i] == '-')
-    {
-        if (str[i] == '-')
-            sign = sign * (-1);
-        i++;
-    }
-    while (str[i] >= 48 && str[i] <= 57)
-    {
-        num = (num * 10) + (str[i] - '0');
-        i++;
-    }
-    return (num * sign);
+	i = 0;
+	num = 0;
+	sign = 1;
+	if (!*str)
+		return (0);
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[i] == '-')
+			sign = sign * (-1);
+		i++;
+	}
+	while (str[i] >= 48 && str[i] <= 57)
+	{
+		num = (num * 10) + (str[i] - '0');
+		i++;
+	}
+	return (num * sign);
 }
 
-long ft_get_time(void)
+long	ft_get_time(void)
 {
-    struct timeval	time;
+	struct timeval	time;
 
-    gettimeofday(&time, NULL);
-    return (time.tv_sec * 1000 + time.tv_usec / 1000);
+	gettimeofday(&time, NULL);
+	return (time.tv_sec * 1000 + time.tv_usec / 1000);
 }
 
-void    ft_message(t_phil *p, char *message)
+void	ft_message(t_phil *p, char *message)
 {
-    pthread_mutex_lock(&p->table->message);
-    if (!p->table->finish)
-        printf("%ld %d %s\n", ft_get_time() - p->table->start_life, p->id,
-               message);
-    pthread_mutex_unlock(&p->table->message);
+	pthread_mutex_lock(&p->table->message);
+	if (!p->table->finish)
+		printf("%ld %d %s\n", ft_get_time() - p->table->start_life, p->id + 1,
+			message);
+	pthread_mutex_unlock(&p->table->message);
 }
 
-int how_much_to_eat(t_phil *p)
+int	how_much_to_eat(t_phil *p)
 {
-    int	i;
+	int	i;
 
-    i = -1;
-    while (++i < p->table->num)
-    {
-        if (p->table->philos[i].eat_count < p->table->must_eat)
-            return (0);
-    }
-    p->table->finish = 1;
-    return (1);
+	i = -1;
+	while (++i < p->table->num)
+	{
+		if (p->table->philos[i].eat_count < p->table->must_eat)
+			return (0);
+	}
+	p->table->finish = 1;
+	return (1);
 }
